3d28383fbec7356c0a7528840b8ae8dd7e166f8b
infoxicator <ruben@infoxication.net>
2020-03-30 14:34:51 +0100

    fix(workflows): env var

--- scripts/run-integration-tests.github.sh
+++ scripts/run-integration-tests.github.sh
@@ -5,7 +5,7 @@ npm run build:sample-modules -- --archive-built-artifacts --bundle-statics-origi
 echo '*' >> sample-module-bundles/CORS && \
 npx surge teardown $SURGE_DOMAIN --token $SURGE_TOKEN && \
 npx surge sample-module-bundles $SURGE_DOMAIN --token $SURGE_TOKEN && \
-docker login -u="$HEROKU_DOCKER_USERNAME" -p="$HEROKU_API_KEY" registry.heroku.com && \
+docker login -u=$HEROKU_DOCKER_USERNAME -p=$HEROKU_API_KEY registry.heroku.com && \
 docker tag one-app:at-test registry.heroku.com/one-app-integration-test/web && \
 docker push registry.heroku.com/one-app-integration-test/web && \
 npx heroku container:release web -a one-app-integration-test && \
