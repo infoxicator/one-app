8c6ec54a350854f494ade730c0f0476b782bc94c
infoxicator <ruben@infoxication.net>
2020-03-30 17:06:49 +0100

    feat(workflow): integration live

--- scripts/run-integration-tests.github.sh
+++ scripts/run-integration-tests.github.sh
@@ -9,4 +9,4 @@ docker login -u=$HEROKU_DOCKER_USERNAME -p=$HEROKU_API_KEY registry.heroku.com &
 docker tag one-app:at-test registry.heroku.com/$HEROKU_APP_ID/web && \
 docker push registry.heroku.com/$HEROKU_APP_ID/web && \
 npx heroku container:release web -a $HEROKU_APP_ID && \
-ONE_DANGEROUSLY_SKIP_ONE_APP_IMAGE_BUILD=true npm run test:integration -- --remote-one-app-environment=$HEROKU_APP_URL
+ONE_DANGEROUSLY_SKIP_ONE_APP_IMAGE_BUILD=true npm run test:integration-live -- --remote-one-app-environment=$HEROKU_APP_URL
