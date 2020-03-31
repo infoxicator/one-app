dee5d8b1ffb6cf6f19db79c4181ac42f3ff80c0e
infoxicator <ruben@infoxication.net>
2020-03-31 12:55:30 +0100

    fix(scripts): chown files

--- prod-sample/generate-certs.sh
+++ prod-sample/generate-certs.sh
@@ -2,9 +2,6 @@
 openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj "/CN=$1" \
   -keyout ./$2/$2-privkey.pem -out ./$2/$2-cert.pem
 
-#chown the certs?
-chown node:node ./$2/$2-cert.pem
-
 # Append cert to aggregate cert file
 cat ./$2/$2-cert.pem >> extra-certs.pem
 # Append a new line
